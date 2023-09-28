db499e888a535594b3bbb0b4d3ebe0a352dc478f
Martins-Jay <matt4real@yahoo.com>
2023-09-28 16:17:25 +0100

    Updated and made some changes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,18 +1,39 @@
 const menuOpenBtn = document.getElementById("menu-open-btn");
 const showNavBar = document.querySelector(".mobile-nav-menu");
 const closeMenuBtn = document.getElementById("menu-close-btn");
+const pageBody = document.querySelector(".main");
+const header = document.querySelector(".main-header");
+const navLink = document.querySelectorAll(".navLink");
 
 const submenuNavList = document.querySelectorAll(".submenu-nav-list");
 
-// OPEN & CLOSE MENU BUTTON
+const hiddenElements = document.querySelectorAll(".hidden");
+
+// OPEN & CLOSE MENU BUTTON ----- NAVIGATION SECTION ------- //
 menuOpenBtn?.addEventListener("click", () => {
-  showNavBar?.classList.toggle("show");
+  showNavBar?.classList.toggle("showMenu");
 });
 
 closeMenuBtn?.addEventListener("click", () => {
-  showNavBar?.classList.toggle("show");
+  showNavBar?.classList.toggle("showMenu");
 });
 
+pageBody?.addEventListener("click", () => {
+  showNavBar?.classList.remove("showMenu");
+});
+
+header?.addEventListener("click", () => {
+  showNavBar?.classList.remove("showMenu");
+});
+
+// CLOSE NAVBAR AFTER SELECTING A NAVLINK
+navLink.forEach((value) => {
+  value.addEventListener("click", () => {
+    showNavBar?.classList.toggle("showMenu");
+  });
+});
+
+// ACCORDION MENU --- REMOVE SHOW
 function removeShow(index) {
   submenuNavList.forEach((value2, index2) => {
     const clickedBtn2 =
@@ -26,7 +47,7 @@ function removeShow(index) {
   });
 }
 
-// ACCORDION MENU
+// ACCORDION MENU --- MAIN FUNCTION
 submenuNavList.forEach((value, index) => {
   let accordionMenu = value.parentElement?.querySelector(".accordion-menu");
   const clickedBtn = value.previousElementSibling?.querySelector(".fa-plus");
@@ -42,4 +63,36 @@ submenuNavList.forEach((value, index) => {
 
     removeShow(index);
   });
+});
+
+// SWIPER JS
+const home_swiper = new Swiper(".home-swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: "true",
+  },
+});
+
+// OBSERVER FOR SWIPER TEXT REVEAL (appearOnSwipe is an object)
+const appearOnSwipe = new IntersectionObserver((observedValues) => {
+  observedValues.forEach((value) => {
+    if (value.isIntersecting) {
+      value.target.classList.add("show");
+    } else {
+      value.target.classList.remove("show");
+    }
+  });
+});
+
+// LETS TELL THE OBSERVER WHAT TO OBSERVE
+hiddenElements.forEach((value) => {
+  appearOnSwipe.observe(value);
 });
